caeb3184ee7a6ac8477d48ed5ba0021202642bf5
Xuan Tho Do <tho.dx@novaintechs.com>
2023-02-24 20:05:10 +0700

    Update Modbus Test - Interval Request Data from Modbus Device

--- Src/app_modbus_test.c
+++ Src/app_modbus_test.c
@@ -16,7 +16,7 @@ enum {
 
 
 static const uint16_t modbus_request_interval = 1000; //1000 ms
-static const uint16_t modbus_app_timeout = 1000; //1000 ms
+static const uint16_t modbus_app_timeout = 5000; //1000 ms
 static const MODBUS_t read_holding_req = {
 		.address = 10,
 		.function_code = MODBUS_READ_INPUT_REGISTER,
@@ -34,10 +34,12 @@ static MODBUS_t response;
 static uint8_t modbus_state = MODBUS_SEND_REQUEST;
 
 void modbus_test_run(){
-	static uint8_t *data_p;
 	static uint32_t start_tx_time = 0;
 	switch (modbus_state) {
 		case MODBUS_SEND_REQUEST:
+			if(HAL_GetTick() - start_tx_time < modbus_request_interval){
+				break;
+			}
 			utils_log_info("MODBUS send request\r\n");
 			start_tx_time = HAL_GetTick();
 			MODBUS_transmit(&read_holding_req);
@@ -48,11 +50,11 @@ void modbus_test_run(){
 				utils_log_info("MODBUS_received:\r\n");
 				utils_log_info("Addr: %d\r\n", response.address);
 				utils_log_info("Function code: %x\r\n", response.function_code);
+				utils_log_info("Byte Cnt: %x\r\n", response.data.read_res.byte_cnt);
 				utils_log_info("Data: [");
-				data_p = (uint8_t*)&response.data;
-				for (size_t i = 0; i < 20; i++)
+				for (size_t i = 0; i < response.data.read_res.byte_cnt; i++)
 				{
-					utils_log_raw("%x", data_p[i]);
+					utils_log_raw("%2x", response.data.read_res.data[i]);
 				}
 				utils_log_raw("]\r\n");
 				modbus_state = MODBUS_SEND_REQUEST;
